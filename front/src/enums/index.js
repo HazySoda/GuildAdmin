48cdb58c3f5f3563803a91345935d4ad2f02fc0b
KidneyFlower <753475777@qq.com>
2018-06-26 17:16:35 +0800

    :sparkles: 增加职业信息和专业技能等相关枚举值

---
+++ front/src/enums/index.js
@@ -0,0 +1,118 @@
+const careerList = [
+  {
+    id: 0,
+    name: '恶魔猎手',
+    color: '#8e41c7'
+  },
+  {
+    id: 1,
+    name: '武僧',
+    color: '#00ffba'
+  },
+  {
+    id: 2,
+    name: '死亡骑士',
+    color: '#c41e3b'
+  },
+  {
+    id: 3,
+    name: '德鲁伊',
+    color: '#ff7c0a'
+  },
+  {
+    id: 4,
+    name: '猎人',
+    color: '#aad372'
+  },
+  {
+    id: 5,
+    name: '法师',
+    color: '#68ccef'
+  },
+  {
+    id: 6,
+    name: '圣骑士',
+    color: '#f48cba'
+  },
+  {
+    id: 7,
+    name: '牧师',
+    color: '#f0ebe0'
+  },
+  {
+    id: 8,
+    name: '潜行者',
+    color: '#fff468'
+  },
+  {
+    id: 9,
+    name: '萨满祭司',
+    color: '#2359ff'
+  },
+  {
+    id: 10,
+    name: '术士',
+    color: '#9382c9'
+  },
+  {
+    id: 11,
+    name: '战士',
+    color: '#c69b6d'
+  }
+]
+
+const skillList = [
+  {
+    id: 0,
+    name: '采矿'
+  },
+  {
+    id: 1,
+    name: '草药学'
+  },
+  {
+    id: 2,
+    name: '剥皮'
+  },
+  {
+    id: 3,
+    name: '锻造'
+  },
+  {
+    id: 4,
+    name: '珠宝'
+  },
+  {
+    id: 5,
+    name: '工程'
+  },
+  {
+    id: 6,
+    name: '制皮'
+  },
+  {
+    id: 7,
+    name: '铭文'
+  },
+  {
+    id: 8,
+    name: '炼金'
+  },
+  {
+    id: 9,
+    name: '裁缝'
+  },
+  {
+    id: 10,
+    name: '附魔'
+  },
+  {
+    id: 11,
+    name: '烹饪'
+  }
+]
+
+export {
+  careerList,
+  skillList
+}
