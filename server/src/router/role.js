10c6848c97b2ffab9e579ab8113d6ac19573b6c9
KidneyFlower <753475777@qq.com>
2018-07-03 16:43:12 +0800

    :sparkles: 角色相关接口开启 Token 验证

--- server/src/router/role.js
+++ server/src/router/role.js
@@ -1,16 +1,16 @@
 const router = require('koa-router')()
 const roleController = require('../controllers/role')
-// const auth = require('../middlewares/auth')
+const auth = require('../middlewares/auth')
 
 router.prefix('/role')
 
 // 添加角色
-router.post('/addRole', roleController.addRole)
+router.post('/addRole', auth, roleController.addRole)
 // 查询角色
-router.post('/queryRole', roleController.queryRole)
+router.post('/queryRole', auth, roleController.queryRole)
 // 修改角色
-router.post('/updateRole', roleController.updateRole)
+router.post('/updateRole', auth, roleController.updateRole)
 // 查询首发名单
-router.post('/queryFirstPublish', roleController.queryFirstPublish)
+router.post('/queryFirstPublish', auth, roleController.queryFirstPublish)
 
 module.exports = router
